f8d20ec52a18a24a257a928a88b3649801aa742f
rob1bouchet <rob1bouchet@gmail.com>
2024-02-25 18:24:27 +0100

    more fixes

--- app/src/main/java/com/example/androidviews/ShowList.kt
+++ app/src/main/java/com/example/androidviews/ShowList.kt
@@ -1,5 +1,3 @@
-import com.example.androidviews.R
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +9,7 @@ import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.androidviews.R
 
 class ShowList : Fragment() {
 
